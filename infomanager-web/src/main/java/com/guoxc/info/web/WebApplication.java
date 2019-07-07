688e496837bbe218ed5c5d48b44d27005946bcf4
guoxczz <guowang456@126.com>
2019-07-07 17:08:29 +0800

    多环境林测试

--- infomanager-web/src/main/java/com/guoxc/info/web/WebApplication.java
+++ infomanager-web/src/main/java/com/guoxc/info/web/WebApplication.java
@@ -16,13 +16,7 @@ public class WebApplication {
 
 	public static void main(String[] args) {
 
-		Properties props=System.getProperties(); //获得系统属性集
-		String osName = props.getProperty("os.name"); //操作系统名称
-		String osArch = props.getProperty("os.arch"); //操作系统构架
-		String osVersion = props.getProperty("os.version"); //操作系统版本
-
-
-//		 SpringApplication.run(WebApplication.class, args);
+		 SpringApplication.run(WebApplication.class, args);
 
 	}
 
